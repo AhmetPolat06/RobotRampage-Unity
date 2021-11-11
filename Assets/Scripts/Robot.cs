183bc5135362396019ec2d8b0cd3af0fe9fd42ac
AhmetPolat06 <savosaren06@gmail.com>
2021-11-10 22:08:36 -0500

    PickUp & Robot spawning logic implemented. Robot sounds effects completed
    Now both pickups and Robots have multiple spawn points and Robots has sounds effects ^-^

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -8,6 +8,12 @@ public class Robot : MonoBehaviour
     GameObject missileprefab;
     [SerializeField]
     private string robotType;
+    [SerializeField]
+    private AudioClip deathSound;
+    [SerializeField]
+    private AudioClip fireSound;
+    [SerializeField]
+    private AudioClip weakHitSound;
     public int health;
     public int range;
     public float fireRate;
@@ -53,6 +59,7 @@ public class Robot : MonoBehaviour
         missile.transform.position = missileFireSpot.transform.position;
         missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
+        GetComponent<AudioSource>().PlayOneShot(fireSound);
     }
 
     // 1
@@ -68,6 +75,11 @@ public class Robot : MonoBehaviour
             isDead = true;
             robot.Play("Die");
             StartCoroutine("DestroyRobot");
+            GetComponent<AudioSource>().PlayOneShot(deathSound);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(weakHitSound);
         }
     }
     // 2
